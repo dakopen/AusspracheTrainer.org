86e717fe7d444279a8aa2495b233326846adc882
Daniel Busch <dakopen185@gmail.com>
2024-07-20 15:48:54 +0200

    add region name

--- aussprachetrainer/aussprachetrainer/settings.py
+++ aussprachetrainer/aussprachetrainer/settings.py
@@ -269,6 +269,7 @@ if USE_AWS:
     AWS_SECRET_ACCESS_KEY = get_secret('AWS-Lightsail-AusspracheTrainer-org-Storage')
     AWS_STORAGE_BUCKET_NAME = 'aussprachetrainer-org-website'
     AWS_S3_SIGNATURE_VERSION = 's3v4'
+    AWS_S3_REGION_NAME = 'eu-central-1'
     AWS_S3_ENDPOINT_URL = 'https://s3.eu-central-1.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
